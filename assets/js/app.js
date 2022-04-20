5faccc3166ad436bf06979d2c692ad12a01b7616
FabioLaForesta <fabiolaforesta@icloud.com>
2022-04-20 15:17:11 +0200

    finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,12 @@
-// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// 1. Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// 2. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 const root = new Vue({
     el: '#root',
 
     data: {
-        text: 'Hello VueJs!'
+        text: 'Hello VueJs!',
+        img: "https://picsum.photos/200/300"
     }
 })
